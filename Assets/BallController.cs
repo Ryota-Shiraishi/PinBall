9a4b3d85ab96dbdc22ce27034f34f8f7e26022c1
Ryota-Shiraishi <54895997+Ryota-Shiraishi@users.noreply.github.com>
2021-02-07 12:26:07 +0900

    得点の表示まで完了

---
+++ Assets/BallController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject ScoreText;
+
+    //スコアの値
+    int Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game\nOver";
+
+            //シーンのリセット
+            if (Input.touchSupported)//タッチに対応しているか判定
+            {
+                if (Input.touchCount > 0)//タッチしているか判定
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+            //タッチ未対応
+            else
+            {
+                if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        //変数Scoreの値を更新
+        switch (other.gameObject.tag)
+        {
+            case "SmallStarTag":
+                Score += 10;
+                break;
+            case "LargeStarTag":
+                Score += 50;
+                break;
+            case "SmallCloudTag":
+                Score += 30;
+                break;
+            case "LargeCloudTag":
+                Score += 100;
+                break;
+        }
+        //ScoreTextの点数を更新
+        this.ScoreText.GetComponent<Text>().text = "Score:" + Score.ToString("00000");
+    }
+}
--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -25,40 +25,11 @@ public class FripperController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //左矢印キーを押した時左フリッパーを動かす
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        //タッチパッドでの操作に対応しているか判定
+        if (Input.touchSupported)
         {
-            SetAngle(this.flickAngle);
-        }
-        //右矢印キーを押した時右フリッパーを動かす
-        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-        //下矢印キーを押した時左右のフリッパーを動かす
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            SetAngle(this.flickAngle);
-        }
-
-        //矢印キー離された時フリッパーを元に戻す
-        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            SetAngle(this.defaultAngle);
-        }
-
-        //タッチパッドでの操作
-        if (Input.touchSupported)//対応しているか判定
-        {
-            if (Input.touchCount > 0)//タッチしているか判定
+            //タッチしているか判定
+            if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);
                 if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
@@ -71,6 +42,38 @@ public class FripperController : MonoBehaviour
                 }
             }
             else
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+        else
+        {
+            //左矢印キーを押した時左フリッパーを動かす        
+            if ((Input.GetKeyDown(KeyCode.LeftArrow)|| Input.GetKeyDown(KeyCode.A)) && tag == "LeftFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+            //右矢印キーを押した時右フリッパーを動かす
+            if ((Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown(KeyCode.D)) && tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+            //下矢印キーを押した時左右のフリッパーを動かす
+            if (Input.GetKeyDown(KeyCode.DownArrow)|| Input.GetKeyDown(KeyCode.S))
+            {
+                SetAngle(this.flickAngle);
+            }
+
+            //矢印キー離された時フリッパーを元に戻す
+            if ((Input.GetKeyUp(KeyCode.LeftArrow)|| Input.GetKeyUp(KeyCode.A)) && tag == "LeftFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+            if ((Input.GetKeyUp(KeyCode.RightArrow)|| Input.GetKeyUp(KeyCode.D)) && tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow)|| Input.GetKeyUp(KeyCode.S))
             {
                 SetAngle(this.defaultAngle);
             }
